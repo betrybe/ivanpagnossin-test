274ed94803d6cfd85ae51a8f84116bf1a582cb33
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2021-09-03 23:00:16 -0300

    Implement XmlImporter

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+
+from inventory_report.helpers.validators import check_unsupported_file
+from inventory_report.importer.importer import Importer
+from inventory_report.models.product import Product
+
+
+class XmlImporter(Importer):
+    def import_data(self, filepath: str) -> list:
+        check_unsupported_file(filepath, '.xml')
+
+        fields = Product.__fields__.keys()
+        tree = ET.parse(filepath)
+        products = [
+            Product(**{f: product.find(f).text for f in fields}).as_dict()
+            for product in tree.getroot()
+        ]
+
+        return products
