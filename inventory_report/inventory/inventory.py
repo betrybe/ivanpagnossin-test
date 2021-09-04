d974896b2d359e207d0650c66bb7e05588913762
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2021-09-03 23:00:50 -0300

    Add support to XML inventory files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -23,7 +24,8 @@ class Inventory():
 
         IMPORTERS = {
             '.csv': CsvImporter,
-            '.json': JsonImporter
+            '.json': JsonImporter,
+            '.xml': XmlImporter
         }
 
         REPORTS = {
